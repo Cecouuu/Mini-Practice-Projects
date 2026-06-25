b20f7b47d07a4dbc7b9a4984802231592b704118
Cecouuu <cvetoslavkrumov2007@gmail.com>
2026-06-25 23:36:11 +0300

    Updated!

--- Practising/scripts/5CharacterCounter.js
+++ Practising/scripts/5CharacterCounter.js
@@ -3,12 +3,20 @@ const output = document.querySelector("#output");
 
 
 input.addEventListener("input", function (){
-    if (!input.value){
-        output.textContent = `Characters = ${input.value.length}`;
-        output.style.color = "red"
-    }else{
-        output.textContent = `Characters = ${input.value.length}`;
-        output.style.color = "green"
-    }
+            if (input.value.length <= 50 && input.value.length >= 0)
+            {
+                output.textContent = `Characters = ${input.value.length}`;
+                output.style.color = "green";
+            }
+            else if (input.value.length >= 51 && input.value.length <= 100)
+            {
+                output.textContent = `Characters = ${input.value.length}`;
+                output.style.color = "#dab600";
+            }
+            else if (input.value.length > 100)
+            {
+                output.textContent = `Characters = ${input.value.length}`;
+                output.style.color = "red";
 
+            }
 })
