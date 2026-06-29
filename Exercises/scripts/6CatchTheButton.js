dc718cbdd206ea3a615562f1691dce29bf33c0c5
Cecouuu <cvetoslavkrumov2007@gmail.com>
2026-06-29 15:41:11 +0300

    New things added!

--- Exercises/scripts/6CatchTheButton.js
+++ Exercises/scripts/6CatchTheButton.js
@@ -1,8 +1,7 @@
-alert("JavaScript Loaded");
-
     const btnCatchMe = document.querySelector("#btnCatchMe");
     const pointsText = document.querySelector("#displayPoints");
     let points = 0;
+    let time = 30;
 
     const gameArea = document.querySelector("#gameArea");
     let btnCatchMeHeight = btnCatchMe.offsetHeight;
@@ -11,16 +10,35 @@ alert("JavaScript Loaded");
     const maxY = gameArea.clientHeight - btnCatchMeHeight;
     const maxX = gameArea.clientWidth - btnCatchMeWidth;
 
+        function moveBtn (){
+            btnCatchMe.style.top = Math.floor((Math.random() * maxY) + 1) + "px";
+            btnCatchMe.style.left = Math.floor((Math.random() * maxX) + 1) + "px";
+        }
 
+        function decreaseBtnSize(){
+
+            if (points >= 10 && points < 20) {
+                btnCatchMe.style.width = btnCatchMeWidth - btnCatchMeWidth * 0.1 + "px";
+                btnCatchMe.style.height = btnCatchMeHeight - btnCatchMeHeight * 0.1 + "px"
+            }
+            else if(points >= 20 && points < 30){
+                btnCatchMe.style.width = btnCatchMeWidth - btnCatchMeWidth * 0.2 + "px";
+                btnCatchMe.style.height = btnCatchMeHeight - btnCatchMeHeight * 0.2 + "px"
+            }
+        }
+
+        function pointsColors (){
+            if (points >= 10 && points < 20){
+
+            }
+        }
 
         btnCatchMe.addEventListener("click", function (){
             points++;
             pointsText.textContent = `Points: ${points}`;
 
-            btnCatchMe.style.top = Math.floor((Math.random() * maxY) + 1) + "px";
-            btnCatchMe.style.left = Math.floor((Math.random() * maxX) + 1) + "px";
-
-
+                moveBtn();
+                decreaseBtnSize();
         })
 
 
